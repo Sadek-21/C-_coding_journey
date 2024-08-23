6eb497740717f17ab483b73a5640266714c4e0e8
SADEK <mohamed20000327@gmail.com>
2024-08-23 01:36:08 +0100

    updt

---
+++ Swap.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+int main() 
+{
+
+vector<int> numbers(5);
+int auxiliary;
+
+for(int i = 0; i < 2; i++) {
+   auxiliary = numbers[i];
+   numbers[i] = numbers[4 - i];
+   numbers[4 - i] = auxiliary;
+}
+}
